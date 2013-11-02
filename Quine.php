0cbdf229da713f2859e3c2ab3e93dd69b94543cf
Jonns Mostovoys <amarr.industrial@gmail.com>
2013-11-02 03:46:37 +0100

    Commentary.

--- Quine.php
+++ Quine.php
@@ -3,11 +3,11 @@ include 'tm.php';
 include 'Utils/MetaProcessor.php';
 include 'Algorithms/Base.php';
 
-/*B::SeekForChar(-1, 1, '!', 
-	B::Move(1, 11, 
-		'HALT'
-	)
-);*/
+/* Surely, this piece of code is not a complete quine in PHP Turing Machine Code Generator.
+   I guess that I got my grade by simply explaining how this thing works, thus lost the interest
+   (because, well, it really works, which is amusing). But now that I shed a manly tear, I want
+   to finish this quine meta-programm as a hat-tip to the school days. */
+
 $seek = B::SeekForChar(-1, 1, '!', 'HALT');
 $move = B::Move(1, 11, $seek);
 
